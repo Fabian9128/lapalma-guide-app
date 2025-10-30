cd79496e58773c093dbae0e1b604402899c6c20f
Fabián González <fabianglezsuarez@gmail.com>
2025-10-30 20:54:02 +0000

    FGO: Fix the header style to be center

--- src/app/pages/restaurantes/restaurantes.component.ts
+++ src/app/pages/restaurantes/restaurantes.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+//MODULES
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-restaurantes',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './restaurantes.component.html',
   styleUrls: ['./restaurantes.component.scss'],
 })
